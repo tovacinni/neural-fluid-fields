10565d5eb70d43d8c200e7371c5fd40926d36ad6
Towaki Takikawa <tovacinni@gmail.com>
2021-12-05 16:56:52 -0500

    Add split optimization losses

--- neuralff/ops/physics_ops.py
+++ neuralff/ops/physics_ops.py
@@ -22,9 +22,189 @@
 import torch
 from .vector_ops import divergence, laplacian, gradient
 
+def time_derivative(
+        coords, velocity_field, timestep, initial_velocity=None):
+    """Computes the time derivative (du/dt).
+
+    Args:
+        coords (torch.Tensor) : coordinates of shape [N, D]
+        velocity_field (neuralff.Field) : velocity field function (aka u)
+        timestep (float) : delta t of the simulation
+        initial_velocity (torch.Tensor) : if provided, will use this instead of sampling the initial velocity.
+                                          This is useful for preconditioning the velocity field.
+
+    Returns:
+        (torch.Tensor, torch.Tensor) : 
+            - time derivative of shape [N, D]
+            - velocity of shape [N, D]
+    """
+    u = velocity_field.sample(coords)
+    if initial_velocity is None:
+        dudt = (u - u.detach()) / timestep
+    else:
+        dudt = (u - initial_velocity) / timestep
+    return dudt, u
+
+def material_derivative(
+        coords, velocity_field, timestep, eps=1e-3, initial_velocity=None):
+    """Computes the material derivative.
+
+    We use the following definition of the material derivative (from Bridson):
+
+        eq 1 : du/dt + (div u) u
+
+    Args:
+        coords (torch.Tensor) : coordinates of shape [N, D]
+        velocity_field (neuralff.Field) : velocity field function (aka u)
+        timestep (float) : delta t of the simulation
+        eps (float) : the "grid spacing" for finite diff
+        initial_velocity (torch.Tensor) : if provided, will use this instead of sampling the initial velocity.
+                                          This is useful for preconditioning the velocity field.
+
+    Returns:
+        (torch.Tensor, torch.Tensor) : 
+            - material derivative of shape [N, 2]
+            - divergence of shape [N, 1]
+    """
+    dudt, u = time_derivative(coords, velocity_field, timestep, initial_velocity=initial_velocity)
+    div_u = divergence(coords, velocity_field, eps=eps)
+    return dudt + div_u * u, div_u
+
+def gravity(coords, timestep):
+    """Returns the gravity vector constraint.
+
+    Args:
+        coords (torch.Tensor) : coordinates of shape [N, D]
+        timestep (float) : delta t of the simulation
+    
+    Returns:
+        (torch.Tensor) : gravity tensor of shape [N, D]
+    """
+    g = torch.zeros_like(coords)
+    g[...,1] = 9.8 * timestep
+    return g
+
+def divergence_free_loss(coords, velocity_field, eps=1e-3)
+    """Computes the divergence-free equation loss.
+
+        eq 1 : div u = 0
+
+    Args:
+        coords (torch.Tensor) : coordinates of shape [N, D]
+        velocity_field (neuralff.Field) : velocity field function (aka u)
+        eps (float) : the "grid spacing" for finite diff
+    
+    Returns:
+        (torch.Tensor) : per-point loss of the divergence-free term of shape [N, 1]
+    """
+    div_u = divergence(coords, velocity_field, eps=eps)
+    return (div_u**2.0).sum(-1, keepdim=True)
+
+def advection_loss(coords, velocity_field, timestep, eps=1e-3, initial_velocity=None):
+    """Computes the advection equation loss.
+
+    We use the following definition (from Bridson)
+
+        eq 1 : du/dt + (div u) u = 0
+    
+    Args:
+        coords (torch.Tensor) : coordinates of shape [N, D]
+        velocity_field (neuralff.Field) : velocity field function (aka u)
+        timestep (float) : delta t of the simulation
+        eps (float) : the "grid spacing" for finite diff
+        initial_velocity (torch.Tensor) : if provided, will use this instead of sampling the initial velocity.
+                                          This is useful for preconditioning the velocity field.
+
+    Returns:
+        (torch.Tensor) : per-point loss of the advection equation of shape [N, 1]
+    """
+    mat_d, div_u = material_derivative(coords, velocity_field, timestep, eps=eps, initial_velocity=initial_velocity)
+    return (mat_d**2.0).sum(-1, keepdim=True)
+
+def body_forces_loss(coords, velocity_field, timestep, eps=1e-3, initial_velocity=None):
+    """Computes the body-forces equation loss.
+
+    We use the following definition (from Bridson)
+
+        eq 1 : du/dt = g
+    
+    Args:
+        coords (torch.Tensor) : coordinates of shape [N, D]
+        velocity_field (neuralff.Field) : velocity field function (aka u)
+        timestep (float) : delta t of the simulation
+        eps (float) : the "grid spacing" for finite diff
+        initial_velocity (torch.Tensor) : if provided, will use this instead of sampling the initial velocity.
+                                          This is useful for preconditioning the velocity field.
+
+    Returns:
+        (torch.Tensor) : per-point loss of the body forces equation of shape [N, 1]
+    """
+    dudt, u = time_derivative(coords, velocity_field, timestep, initial_velocity=initial_velocity)
+    g = gravity(coords, timestep)
+    return ((g - dudt)**2.0).sum(-1, keepdim=True)
+
+def incompressibility_loss(coords, pressure_field, timestep, eps=1e-3, rho=1e-3, initial_velocity=None):
+    """Computes the incompressibility equation loss.
+
+    We use the following definition (from Bridson)
+
+        eq 1 : du/dt = (1/rho) grad p
+        eq 2 : div u = 0
+
+    Args:
+        coords (torch.Tensor) : coordinates of shape [N, D]
+        pressure_field (neuralff.Field) : pressure field function (aka p)
+        timestep (float) : delta t of the simulation
+        eps (float) : the "grid spacing" for finite diff
+        initial_velocity (torch.Tensor) : if provided, will use this instead of sampling the initial velocity.
+                                          This is useful for preconditioning the velocity field.
+
+    Returns:
+        (torch.Tensor) : per-point loss of the incompressibility equation of shape [N, 1]
+    """
+    dudt, u = time_derivative(coords, velocity_field, timestep, initial_velocity=initial_velocity)
+    div_u = divergence(coords, velocity_field, eps=eps)
+    grad_p = (1.0/rho) * gradient(coords, pressure_field, eps=eps)
+    return ((grad_p - dudt)**2.0).sum(-1, keepdim=True) + (div_u**2.0).sum(-1, keepdim=True)
+
+def euler_loss(
+        coords, velocity_field, pressure_field, 
+        timestep, rho=1e-3, eps=1e-3, initial_velocity=None):
+    """Computes the incompressible Euler equation loss.
+
+    We use the following definition of the Euler equation (from Bridson):
+    
+        eq 1 : du/dt + (div u) u + (1/rho) grad p = g
+        eq 2 : div u = 0
+
+    That is, this is the Navier-Stokes equation without the viscosity term.
+
+    Args:
+        coords (torch.Tensor) : coordinates to enforce Navier-Stokes of shape [N, D]
+        velocity_field (neuralff.Field) : velocity field function (aka u)
+        pressure_field (neuralff.Field) : pressure field function (aka p)
+        timestep (float) : delta t of the simulation
+        rho_field (neuralff.Field) : the fluid density field (kg/m^d)
+        eps (float) : the "grid spacing" for finite diff
+        initial_velocity (torch.Tensor) : if provided, will use this instead of sampling the initial velocity.
+                                          This is useful for preconditioning the velocity field.
+    
+    Returns:
+        (torch.Tensor) : per-point loss of the Euler equation of shape [N, 1]
+    """
+    mat_d, div_u = material_derivative(coords, velocity_field, timestep, eps=eps, initial_velocity=initial_velocity)
+
+    grad_p = (1.0/rho) * gradient(coords, pressure_field, eps=eps)
+    
+    g = gravity(coords, timestep)
+    
+    momentum_term = ((g - mat_d - grad_p)**2).sum(-1, keepdim=True)
+    divergence_term = (div_u**2).sum(-1, keepdim=True)
+    return momentum_term + divergence_term
+
 def navier_stokes_loss(
         coords, velocity_field, pressure_field, 
-        timestep, rho=1e-2, nu=1e-5, eps=1e-3, initial_velocity=None):
+        timestep, rho=1e-0, nu=1e-5, eps=1e-3, initial_velocity=None):
     """Computes the Navier Stokes equation loss.
 
     We use the following definition of the Navier-Stokes equation (from Bridson):
@@ -37,7 +217,7 @@ def navier_stokes_loss(
         velocity_field (neuralff.Field) : velocity field function (aka u)
         pressure_field (neuralff.Field) : pressure field function (aka p)
         timestep (float) : delta t of the simulation
-        rho (float) : the fluid density (kg/m^d)
+        rho (neuralff.Field) : the fluid density field (kg/m^d)
         nu (float) : kinematic viscosity
         eps (float) : the "grid spacing" for finite diff
         initial_velocity (torch.Tensor) : if provided, will use this instead of sampling the initial velocity.
@@ -46,27 +226,15 @@ def navier_stokes_loss(
     Returns:
         (torch.Tensor) : per-point loss of the Navier-Stokes equation of shape [N, 1]
     """
-    u = velocity_field.sample(coords)
-
-    div_u = divergence(coords, velocity_field, eps=eps)
-
-    if initial_velocity is None:
-        dudt = (u - u.detach()) / timestep
-    else:
-        dudt = (u - initial_velocity) / timestep
+    mat_d, div_u = material_derivative(coords, velocity_field, timestep, eps=eps, initial_velocity=initial_velocity)
 
     grad_p = (1.0/rho) * gradient(coords, pressure_field, eps=eps)
     
-    g = torch.zeros_like(coords)
-    g[...,1] = 9.8 * timestep
+    g = gravity(coords, timestep)
 
     diff = nu * laplacian(coords, velocity_field, eps=eps)
     
-
-    momentum_term = ((g + diff - dudt - div_u * u - grad_p)**2).sum(-1, keepdim=True)
-    #momentum_term = ((g-dudt)**2).sum(-1, keepdim=True)
+    momentum_term = ((g + diff - mat_d - grad_p)**2).sum(-1, keepdim=True)
     divergence_term = (div_u**2).sum(-1, keepdim=True)
     return momentum_term + divergence_term
-
-
 
